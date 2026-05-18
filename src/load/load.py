c7e0df2215980263df8a5e4972bd4f21f8bfd118
GuilhermeSoutoS <guilherme.ssouza@sptech.school>
2026-05-18 02:16:04 -0300

    ETL 100% refatorada utlizando biblioteca Pandas para sustentar melhor o grupo

--- src/load/load.py
+++ src/load/load.py
@@ -1,14 +1,23 @@
 import mysql.connector
 import os
 from dotenv import load_dotenv
-import csv
-import boto3
+import pandas as pd
 import json
+from datetime import datetime
+import boto3
 
-#VERSÃO BASE DO LOAD
-#Lê o .env e traz as credenciais do banco 
 load_dotenv()
 
+BUCKET = os.getenv('AWS_BUCKET_NAME')
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    aws_session_token=os.getenv('AWS_SESSION_TOKEN')
+)
+
+hoje = datetime.now().strftime('%d_%m_%Y')
 
 conexao = mysql.connector.connect(
     host=os.getenv('DBHOST'),
@@ -17,81 +26,115 @@ conexao = mysql.connector.connect(
     database=os.getenv('DBNAME')
 )
 
-dadosClient = []
-registros = {}
-
-#Cursor é o objeto que executa funções no banco
 cursor = conexao.cursor()
 
-with open('dadosFinais.csv', 'r', encoding="utf-8") as arquivo:
-    leitura = csv.DictReader(arquivo)
+dadosMaquinas = f'dados_tratados_{hoje}.csv'
+dadosProcessos = f'processos_tratados_{hoje}.csv'
 
-    for linha in leitura:
-        macAddres = linha['mac']
-       
-        #Percorre linha por linha e retorna o resultado desse select
-        cursor.execute(
-            "SELECT idDisplay, fkEmpresa from display where mac_addres = %s",(macAddres,)
-        )
+# Lê os CSVs tratados e armazena na variável
+dados = pd.read_csv(dadosMaquinas, encoding='utf-8')
+processos = pd.read_csv(dadosProcessos, encoding='utf-8')
 
-        #Traz o resultado e guarda na variável
-        resultado = cursor.fetchone()
+# Cria o top 5 processos que mais consomem memória RAM (segui o padrão do gerenciador de tarefas)
+#Sort_values = ordena do MAIOR uso de RAM para o MENOR
+#groupby = Como ele trata todos os processos do arquivo ele já guarda de uma vez só o top 5 de cada máquina usando como parametro o macAdress, ip e dataHora
+#head(5) = guarde os 5 primeiros de cada maquina
+processos_top5 = (
+    processos.sort_values('usoMemoriaProcessoMB', ascending=False).groupby(['mac', 'ip', 'data_hora']).head(5)
+)
 
-        if resultado is not None:
-            idDisplay = resultado[0]
-            fkEmpresa = resultado[0]
+# Junta dados da máquina com os processos
+arquivoFinal = pd.merge(
+    dados,
+    processos_top5,
+    on=['mac', 'ip', 'data_hora'],
+    how='inner'
+)
 
-            chave = (idDisplay, linha["data_hora"])
-            if chave not in registros:
+#Criar um json por empresa por motivos de segurança
+empresas = {}
 
-            #Monta o dicionário com o formato que queremos
-                registros[chave] = {
-                "idDisplay": idDisplay,
-                "idEmpresa": fkEmpresa,
-                "macAddres": macAddres,
-                "dataHora": linha["data_hora"],
-                "cpu%": float(linha["cpu_percentual"]),
-                "ram%": float(linha["ram_percentual"]),
-                "ramTotal": float(linha["ram_total_gb"]),
-                "ramUsada": float(linha["ram_total_gb"]) - float(linha["ram_disponivel_gb"]),
-                "disco%": float(linha["disco_percentual"]),
-                "total_disco_gb": float(linha["total_disco_gb"]),
-                "disco_usado_gb": float(linha["disco_usado_gb"]),
-                "upload_mb":float(linha["upload_mb"]),
-                "download_mb":float(linha["download_mb"]),
-                "top5processos": []
+#A função iterrow retorna um indice e uma linha (recomendo pesquisar se quiser entender esse For)
+#Mas como não queremos trabalhar com o indice de cada linha, usei o _, pra pular o primeiro valor
+#E usar só o objeto retornado (a linha no caso)
+for _, linha in arquivoFinal.iterrows():
+    macAddres = linha['mac']
 
-            }
+    cursor.execute(
+        "SELECT idDisplay, fkEmpresa FROM display WHERE mac_addres = %s",
+        (macAddres,)
+    )
 
-            processo = {
-            "pid": int(linha["pid"]),
-            "usuario": linha["usuario"],
-            "nomeProcesso": linha["nomeProcesso"],
-            "usoMemoriaProcesso": float(linha["usoMemoriaProcessoMB"]),
-            "usoCpuProcesso": float(linha["usoCpuProcesso"])
+    resultado = cursor.fetchone()
+
+    if resultado is None:
+        print(f"MAC não encontrado no banco: {macAddres}")
+        continue
+
+    idDisplay = resultado[0]
+    fkEmpresa = str(resultado[1])
+
+    display_minuto = (idDisplay, linha['data_hora'])
+
+    if fkEmpresa not in empresas:
+        empresas[fkEmpresa] = {}
+
+    if display_minuto not in empresas[fkEmpresa]:
+        empresas[fkEmpresa][display_minuto] = {
+            "idDisplay": int(idDisplay),
+            "idEmpresa": int(fkEmpresa),
+            "macAddres": macAddres,
+            "dataHora": linha["data_hora"],
+
+            "cpu%": float(linha["cpu_percentual"]),
+            "ram%": float(linha["ram_percentual"]),
+            "ramTotal": float(linha["ram_total_gb"]),
+            "ramUsada": round(float(linha["ram_total_gb"]) - float(linha["ram_disponivel_gb"]),2
+            ),
+
+            "disco%": float(linha["disco_percentual"]),
+            "total_disco_gb": float(linha["total_disco_gb"]),
+            "disco_usado_gb": float(linha["disco_usado_gb"]),
+
+            "upload_mb": float(linha["upload_mb"]),
+            "download_mb": float(linha["download_mb"]),
+
+            "top5processos": []
         }
 
-            registros[chave]["top5processos"].append(processo)
+    processo = {
+        "pid": int(linha["pid"]),
+        "usuario": linha["usuario"],
+        "nomeProcesso": linha["nomeProcesso"],
+        "usoMemoriaProcesso": float(linha["usoMemoriaProcessoMB"]),
+        "usoCpuProcesso": float(linha["usoCpuProcesso"])
+    }
 
-        else:
-            print(f"MAC não encontrado no banco: {macAddres}")
+    empresas[fkEmpresa][display_minuto]["top5processos"].append(processo)
 
-dadosClient = list(registros.values())
-print(linha["nomeProcesso"])
 
-#Finaliza a Query 
 cursor.close()
 conexao.close()
 
-#Crio o .json com os dados coletados
-with open('dados_dashboard.json', "w", encoding="utf-8") as arquivoJson:
-    json.dump(dadosClient, arquivoJson,indent=4, ensure_ascii=False)
 
-#s3 = boto3.client('s3', 
-   # aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-   # aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    #aws_session_token=os.getenv('AWS_SESSION_TOKEN')
-#)
+# Cria um JSON separado para cada empresa
+for idEmpresa, registros in empresas.items():
+    dados = list(registros.values())
 
-#s3.upload_file('dados_dashboard.json','vooh-bucket','client/dados_dashboard.json')
-print("Upload feito com sucesso!")
+    nomeArquivo = f'dados_dashboard_empresa_{idEmpresa}_{hoje}.json'
+
+    with open(nomeArquivo, 'w', encoding='utf-8') as arquivoJson:
+        json.dump(
+            dados,
+            arquivoJson,
+            indent=4,
+            ensure_ascii=False
+        )
+
+        
+    s3.upload_file(
+        nomeArquivo,
+        BUCKET,
+        f'client/{nomeArquivo}'
+    )
+    print(f"JSON criado: {nomeArquivo}")
--- src/transform/transform.py
+++ src/transform/transform.py
@@ -1,121 +1,145 @@
-import csv
+import pandas as pd
+import glob
 from datetime import datetime
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+hoje = datetime.now().strftime('%d_%m_%Y')
+
+#O script de captura salva os processos e os dados da máquinas em segundos diferentes, e isso retorna um erro na 
+#hora de juntar os dois csv's lá na frente. Então trunquei para que valide somente a hora e minuto
+def truncar_minuto(coluna):
+    return pd.to_datetime(
+        coluna,
+        format='%d-%m-%Y %H:%M:%S'
+    ).dt.strftime('%d-%m-%Y %H:%M')
 
 
-def truncar_minuto(data_hora_str):
-    dt = datetime.strptime(data_hora_str, '%d-%m-%Y %H:%M:%S')
-    return dt.strftime('%d-%m-%Y %H:%M')
+#Utilizei a biblioteca Glob que verifica todos os dados em uma determinada pasta que possuem um nome padrão
+#e depois faz a leitura de cada arquivo csv e guarda na variável
+#Fiz uma tratativa de erro pra ficar mais profissional
 
-vetorTratado = []
+try:
 
-with open('data/dados_16_04_2026_D0001.csv' , 'r', encoding='utf-8') as arquivo:
-    leitura = csv.DictReader(arquivo)
+    dadosMaquinas = glob.glob(f'data/dados_{hoje}_*.csv')
 
-    for linha in leitura:
-        
-        dado_tratado = {
-            'data_hora': truncar_minuto(linha['data_hora']),
-            
-            #Converter dados de disco
-            'total_disco_gb': round(int(linha['total_disco']) / (1024 ** 3), 2),
-            'disco_usado_gb': round(int(linha['disco_usado']) / (1024 ** 3), 2),
-            'disco_livre_gb': round(int(linha['disco_livre']) / (1024 ** 3), 2),
-            'disco_percentual': float(linha['disco_percentual']),
+    if not dadosMaquinas:
+        raise FileNotFoundError(
+            f'Nenhum arquivo de dados de Máquina encontrado para {hoje}'
+        )
 
-            #Converter dados de CPU
-            'processador_nome': linha['processador_nome'],
-            'nucleos_fiscos': linha['nucleos_fiscos'],
-            'nucleos_totais': linha['nucleos_totais'],
-            #'frequencia_min_ghz': round(float(linha['frequencia_min']) / 1000, 2),
-            'frequencia_max_ghz': round(float(linha['frequencia_max']) / 1000, 2),
-            'frequencia_atual_ghz': round(float(linha['frequencia_atual']) / 1000, 2),
-            'cpu_percentual': float(linha['cpu_percentual']),
+    csvDados = pd.concat(
+        [pd.read_csv(arq, encoding='utf-8')
+        for arq in dadosMaquinas],
+        ignore_index=True
+    )
 
-            #Converter dados de RAM
-            'ram_total_gb': round(float(linha['ram_total']) / (1024 ** 3),2),
-            'ram_disponivel_gb':round(float(linha['ram_disponivel']) / (1024 ** 3),2),
-            'ram_percentual': float(linha['ram_percentual']),
+    dadosProcessos = glob.glob(f'data/processos_{hoje}_*.csv')
 
-            #Converter os dados de rede
-            'upload_mb': round(int(linha['upload']) / (1024 ** 2), 2),
-            'download_mb': round(int(linha['download']) / (1024 ** 2), 2),
+    if not dadosProcessos:
+        raise FileNotFoundError(
+            f'Nenhum arquivo de processos encontrado para {hoje}'
+        )
 
-            'mac': linha['mac'],
-            'ip': linha['ip']
-        }
-        vetorTratado.append(dado_tratado)
+    csvProcessos = pd.concat(
+        [pd.read_csv(arq, encoding='utf-8') 
+        for arq in dadosProcessos],
+        ignore_index=True
+    )
 
-vProcessosTratados = []
-
-with open('data/processos_16_04_2026_D0001.csv' , 'r', encoding='utf-8') as arquivo:
-    leitura = csv.DictReader(arquivo)
-
-    for linha in leitura:
-        
-
-        processosTratados = {
-            'data_hora': truncar_minuto(linha['data_hora']),
-            "pid": linha['pid'],
-            'usuario': linha['usuario'],
-            'nomeProcesso': linha['nome'],
-            'usoMemoriaProcessoMB': round(int(linha['memoria']) / (1024 ** 2), 2),
-            'usoCpuProcesso': linha['uso_cpu'],
-            'mac': linha['mac'],
-            'ip': linha['ip']
-        }
-        vProcessosTratados.append(processosTratados)
+    
 
 
-colunas = list(vetorTratado[0].keys())
 
-with open('dadosTratados.csv', 'w', newline='', encoding='utf-8') as arq:
-    writer = csv.DictWriter(arq, fieldnames=colunas)
-    writer.writeheader()
-    writer.writerows(vetorTratado)
+#Diferente da biblioteca CSV, o Pandas faz o tratamento por colunas, evitando a necessidade de uso do For
+    dados = pd.DataFrame({
+        'data_hora': truncar_minuto(csvDados['data_hora']),
 
+     #Converter dados de disco
+        'total_disco_gb': (csvDados['total_disco'].astype(int) / (1024 ** 3)).round(2),
+        'disco_usado_gb': (csvDados['disco_usado'].astype(int) / (1024 ** 3)).round(2),
+        'disco_livre_gb': (csvDados['disco_livre'].astype(int) / (1024 ** 3)).round(2),
+        'disco_percentual': csvDados['disco_percentual'].astype(float),
 
-colunas = list(vProcessosTratados[0].keys())
-with open('processosTratados.csv', 'w', newline='', encoding='utf-8') as arq:
-    writer = csv.DictWriter(arq, fieldnames=colunas)
-    writer.writeheader()
-    writer.writerows(vProcessosTratados)
+    #Converter dados de CPU
+        'processador_nome': csvDados['processador_nome'],
+        'nucleos_fiscos': csvDados['nucleos_fiscos'],
+        'nucleos_totais': csvDados['nucleos_totais'],
+        'frequencia_max_ghz': (csvDados['frequencia_max'].astype(float) / 1000).round(2),
+        'frequencia_atual_ghz': (csvDados['frequencia_atual'].astype(float) / 1000).round(2),
+        'cpu_percentual': csvDados['cpu_percentual'].astype(float),
 
+    #Converter dados de RAM
+        'ram_total_gb': (csvDados['ram_total'].astype(float) / (1024 ** 3)).round(2),
+        'ram_disponivel_gb': (csvDados['ram_disponivel'].astype(float) / (1024 ** 3)).round(2),
+        'ram_percentual': csvDados['ram_percentual'].astype(float),
 
-#Pega o top5 processo que mais usa ram ordenados do maior para o menor
-processos_por_maquina = {}
+    #Converter os dados de rede
+        'upload_mb': (csvDados['upload'].astype(int) / (1024 ** 2)).round(2),
+        'download_mb': (csvDados['download'].astype(int) / (1024 ** 2)).round(2),
 
-for processo in vProcessosTratados:
-    chave = (processo['mac'], processo['ip'], processo['data_hora'])
-
-    if chave not in processos_por_maquina:
-        processos_por_maquina[chave] = []
-
-    processos_por_maquina[chave].append(processo)
-
-processos_relevantes = []
-
-for chave, lista in processos_por_maquina.items():
-    top5 = sorted(
-        lista,
-        key=lambda p: p['usoMemoriaProcessoMB'],
-        reverse=True
-    )[:5]
-
-    processos_relevantes.extend(top5)
+        'mac': csvDados['mac'],
+        'ip': csvDados['ip']
+    })
 
 
-# Junta os dois vetores pelo mac e ip
-vetorFinal = []
+#Tratamento dos processos
+    processos = pd.DataFrame({
+        'data_hora': truncar_minuto(csvProcessos['data_hora']),
+        'pid': csvProcessos['pid'],
+        'usuario': csvProcessos['usuario'],
+        'nomeProcesso': csvProcessos['nome'],
+        'usoMemoriaProcessoMB': (csvProcessos['memoria'].astype(int) / (1024 ** 2)).round(2),
+        'usoCpuProcesso': csvProcessos['uso_cpu'],
+        'mac': csvProcessos['mac'],
+        'ip': csvProcessos['ip']
+    })
 
-for processo in processos_relevantes:
-    for dado in vetorTratado:
-        if processo['mac'] == dado['mac'] and processo['ip'] == dado['ip']and processo['data_hora'] == dado['data_hora']:
-            linha_final = {**dado, **processo}
-            vetorFinal.append(linha_final)
 
-# Escreve o CSV final
-colunas = list(vetorFinal[0].keys())
-with open('dadosFinais.csv', 'w', newline='', encoding='utf-8') as arq:
-    writer = csv.DictWriter(arq, fieldnames=colunas)
-    writer.writeheader()
-    writer.writerows(vetorFinal)
+
+    dados.to_csv(
+        f'dados_tratados_{hoje}.csv',
+        index=False,
+        encoding='utf-8'
+    )
+
+    processos.to_csv(
+        f'processos_tratados_{hoje}.csv',
+        index=False,
+            encoding='utf-8'
+    )
+
+    arquivoDados = f'dados_tratados_{hoje}.csv'
+    arquivoProcessos = f'processos_tratados_{hoje}.csv'
+
+    s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    aws_session_token=os.getenv('AWS_SESSION_TOKEN')
+    )
+
+    BUCKET = os.getenv('AWS_BUCKET_NAME')
+
+    s3.upload_file(
+        arquivoDados,
+        BUCKET,
+        f'trusted/{arquivoDados}'
+    )
+
+    s3.upload_file(
+        arquivoProcessos,
+        BUCKET,
+        f'trusted/{arquivoProcessos}'
+    )
+
+    print("Tratamento de dados finalizado com sucesso!")
+
+    
+except FileNotFoundError as erro:
+    print(erro)
+
+    
